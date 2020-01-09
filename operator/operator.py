1fdc56462b1316908638d753c8447d3b0cacd226
Bongani NKOSI <bnkosi@c5r11s8.wethinkcode.co.za>
2020-01-09 17:13:24 +0200

    added input areas and heading in POS main screen

--- operator/operator.py
+++ operator/operator.py
@@ -4,7 +4,7 @@ from kivy.uix.boxlayout import BoxLayout
 class OperatorWindow(BoxLayout):
     def __init__(self, **kwargs):
         super(). __init__(**kwargs)
-            
+
 class OperatorApp(App):
         def build(self):
             return OperatorWindow()
--- signin/signin.py
+++ signin/signin.py
@@ -6,21 +6,21 @@ import re
 class SigninWindow(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-    
+
     def validate_user(self):
         errors = ''
         user = self.ids.username_field
         password = self.ids.pwd_field
         info = self.ids.info
-        
+
         uname = user.text
         pwd = password.text
-        
+
         if uname == '' or pwd == '':
             errors += 'Username and Password are required to sign in.\n'
         elif uname != 'admin' or pwd != 'admin':
             errors += 'Invalid Username or Password'
-        
+
         if errors:
             info.text = (f'[color=#FF0000]{errors}[/color]')
         else:
