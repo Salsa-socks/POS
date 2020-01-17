48815c405c9888d05b78cb080051ff350b6ce980
bnkosi2019 <47521522+bnkosi2019@users.noreply.github.com>
2020-01-17 17:17:02 +0200

    need to fix totals next

--- operator/operator.py
+++ operator/operator.py
@@ -1,13 +1,17 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+import re
 
 class OperatorWindow(BoxLayout):
     def __init__(self, **kwargs):
         super(). __init__(**kwargs)
         
+        self.cart = [] #contains all items selected
+        self.qty = []
+        
     def update_purchases(self):
-        code = self.ids.code_inp.text
+        pcode = self.ids.code_inp.text
         qty = self.ids.qty_inp.text
         name = self.ids.code_inp.text
         disc = self.ids.disc_inp.text
@@ -15,11 +19,11 @@ class OperatorWindow(BoxLayout):
         total = self.ids.total_inp.text
         
         products_container = self.ids.products
-        if code and qty and name: #needs some kind of validation checks eg: numbers inputted, range for percs
+        if pcode =='1234' or pcode == '2345': #needs some kind of validation checks eg: numbers inputted, range for percs
             details = BoxLayout(size_hint_y=None, height=30, pos_hint={'top':1})
             products_container.add_widget(details)
             
-            code = Label(text=code, size_hint_x=.2, color=(.06, .45, .45, 1))
+            code = Label(text=pcode, size_hint_x=.2, color=(.06, .45, .45, 1))
             name = Label(text='Product', size_hint_x=.3, color=(.06, .45, .45, 1))
             qty = Label(text=qty, size_hint_x=.1, color=(.06, .45, .45, 1))
             disc = Label(text=f'{disc}%', size_hint_x=.1, color=(.06, .45, .45, 1))
@@ -34,16 +38,35 @@ class OperatorWindow(BoxLayout):
             details.add_widget(total)
             
             # update Preview
-            pname = self.ids.code_inp.text
-            pprice = f'R{self.ids.price_inp.text}'
+            pname = "Product One"
+            if pcode == '2345':
+                pname = 'Product Two'
+            pprice = 1.00
+            pqty = str(1)
             preview = self.ids.receipt_preview
             prev_text = preview.text
+            purchase_total = '`\n\nTotal\t\t\t\t0.00'
             _prev = prev_text.find('`')
             if _prev > 0:
-                prev_text = prev_text[:_prev]
-            purchase_total = '`\n\nTotal\t\t\t\t0.00'
-            new_preview = '\n'.join([prev_text, pname+'\t\t\t\t'+str(pprice),purchase_total])
-            preview.text = new_preview
+                prev_text = prev_text[:_prev]           
+            
+            ptarget = -1
+            for i,c in enumerate(self.cart):
+                if c == pcode:
+                    ptarget = i
+                    
+            if ptarget >= 0:
+                pqty = self.qty[ptarget]+1
+                self.qty[ptarget] = pqty
+                expr = '%s\t\tX\d\t'%(pname)
+                rexpr = pname+'\t\tX'+str(pqty)+'\t'
+                new_text = re.sub(expr, rexpr, prev_text)
+                preview.text = new_text
+            else:
+                self.cart.append(pcode)
+                self.qty.append(1)
+                new_preview = '\n'.join([prev_text, pname+'\t\tX'+pqty+'\t\t'+str(pprice),purchase_total])
+                preview.text = new_preview
 
 class OperatorApp(App):
         def build(self):
